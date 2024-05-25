ad560f823e554353e044f50e5f658cbaa6fb4e03
Alejandro Arias <alejandro77arias@gmail.com>
2024-05-25 18:46:43 -0500

    create users and polymorphism

--- src/model/login.py
+++ src/model/login.py
@@ -1,11 +1,28 @@
+from src.classes.Profesor import Profesor
+from src.classes.Estudiante import Estudiante
+from src.classes.Administrador import Administrador
+from src.classes.Usuario import checkUsuario
 
-
-def login():
+def login():   
     while True:
         userName = input("Ingrese su usuario: ")
         password = input("Ingrese su contraseña: ")
-    try:
-        userName == 
-    except 
+        tipo, user = checkUsuario(userName,password)
+        currentUser = None
+        if(tipo == "administrador"):
+            currentUser = Administrador(user.userName,user.id,
+                user.password,user.nombre,user.edad,user.genero,
+                user.direccion,user.cellphone,user.email)
+        elif(tipo == "estudiante"):
+            currentUser = Estudiante(user.userName,user.id,
+                user.password,user.nombre,user.edad,user.genero,
+                user.direccion,user.cellphone,user.email)
+        elif(tipo == "profesor"):
+            currentUser = Profesor(user.userName,user.id,
+                user.password,user.nombre,user.edad,user.genero,
+                user.direccion,user.cellphone,user.email)
+        else:
+            print("El usuario no existe")
+        print(currentUser)
 
 
