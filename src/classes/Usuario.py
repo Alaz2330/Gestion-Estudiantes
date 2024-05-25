b402b18121ba4114f71801015c7a0ae8e6290a94
Alejandro Arias <alejandro77arias@gmail.com>
2024-05-24 23:44:26 -0500

    Class structure

---
+++ src/classes/Usuario.py
@@ -0,0 +1,11 @@
+class Usuario:
+    def __init__(self, user: str, id: str, psd: str, nombre: str, edad: str, genero: str, direccion: str, cnumber: str, email: str):
+        self.user = user
+        self.id = id
+        self.psd = psd
+        self.nombre = nombre
+        self.edad = edad 
+        self.genero = genero 
+        self.direccion = direccion
+        self.cnumber = cnumber
+        self.email = email
